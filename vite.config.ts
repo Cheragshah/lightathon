4e39f0607ccb6229597da659a601045f854b46f5
Cheragshah <cheragshah@hotmail.com>
2026-01-05 11:03:19 +0530

    Refactor vite.config.ts to use object syntax and host: true

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig(() => ({
-  plugins: [react()],
+export default defineConfig({
   server: {
-    host: "0.0.0.0",
+    host: true,
     port: 3000,
   },
   preview: {
-    host: "0.0.0.0",
+    host: true,
     port: 3000,
   },
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
