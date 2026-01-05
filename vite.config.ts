0c9161b2d17aa4251d4f4e767c4728055eb45ac6
Cheragshah <cheragshah@hotmail.com>
2026-01-05 10:57:09 +0530

    Update vite config host to 0.0.0.0 and port to 3000

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
+  plugins: [react()],
   server: {
-    host: "::",
-    port: 8080,
+    host: "0.0.0.0",
+    port: 3000,
   },
-  plugins: [react()].filter(Boolean),
+  preview: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
