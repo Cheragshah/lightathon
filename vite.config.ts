4badd76fda1f0fb54a1ce75102e1011a5fb356a8
Cheragshah <cheragshah@hotmail.com>
2026-01-05 13:47:37 +0530

    Update allowedHosts to businessfreedommodel.com and node version

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,12 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
-    allowedHosts: ["codexalpha.cloud", "www.codexalpha.cloud"]
+    allowedHosts: ["businessfreedommodel.com", "www.businessfreedommodel.com"]
   },
   preview: {
     host: true,
     port: 3000,
-    allowedHosts: ["codexalpha.cloud", "www.codexalpha.cloud"]
+    allowedHosts: ["businessfreedommodel.com", "www.businessfreedommodel.com"]
   },
   plugins: [react()],
   resolve: {
